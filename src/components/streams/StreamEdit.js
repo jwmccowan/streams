00299a342adb039cc1227dc1cce8310041334bdd
jwmccowan <jwmccowan@gmail.com>
2019-09-17 03:06:33 +1000

    stream creation is finished

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
-  return (
-    <div>
-      StreamEdit
-    </div>
-  );
-};
+import { fetchStream } from '../../actions';
 
-export default StreamEdit;
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    const { fetchStream, match } = this.props;
+    fetchStream(match.params.id);
+  }
+
+  render() {
+    const {stream} = this.props;
+    if (!stream) {
+      return <div>Loading..</div>;
+    }
+    return (
+      <div>
+        {stream.title}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ streams }, ownProps) => {
+  return {stream: streams[ownProps.match.params.id]};
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchStream },
+)(StreamEdit);
